d66a9567fa110070a9be6868a5ab789d6bd9bcb6
AvengerEug <eugenesumarry@163.com>
2019-08-11 23:13:21 +0800

    springboot 集成rabbit-mq 第一弹

---
+++ springboot-rabbit-mq/springboot-rabbit-mq-01/src/main/java/com/eugene/sumarry/sbrabbitmq/listener/PushOrderRecordListener.java
@@ -0,0 +1,38 @@
+package com.eugene.sumarry.sbrabbitmq.listener;
+
+import com.eugene.sumarry.sbrabbitmq.Entity.OrderRecord;
+import com.eugene.sumarry.sbrabbitmq.listener.event.PushOrderRecordEvent;
+import com.eugene.sumarry.sbrabbitmq.mapper.OrderRecordMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 这就是监听器-跟RabbitMQ的Listener几乎是一个理念
+ */
+@Component
+public class PushOrderRecordListener implements ApplicationListener<PushOrderRecordEvent> {
+
+    private static final Logger log = LoggerFactory.getLogger(PushOrderRecordListener.class);
+
+    @Autowired
+    private OrderRecordMapper orderRecordMapper;
+
+    @Override
+    public void onApplicationEvent(PushOrderRecordEvent event) {
+        log.info("监听到的下单记录： {} ", event);
+
+        try {
+            if (event != null) {
+                OrderRecord entity = new OrderRecord();
+                BeanUtils.copyProperties(event, entity);
+                orderRecordMapper.insertSelective(entity);
+            }
+        } catch (Exception e) {
+            log.error("监听下单记录发生异常：{} ", event, e.fillInStackTrace());
+        }
+    }
+}
